c4ebd3875cc16b7d381b2679faaa10568647fd79
HectorDEV123 <trashdev097@gmail.com>
2023-02-12 15:29:31 -0500

    Updated

--- javascript/LocalDb/DBINIT.js
+++ javascript/LocalDb/DBINIT.js
@@ -1,97 +1,23 @@
-import { PlayListArrayOne, PlayListArrayTwo, PlayListArrayThree } from "../script.js";
+// ! The Import Statement imports the Stringified Version of the Array
 
-export function CheckDataBase(){
-  if (localStorage.length == 0){
-    console.log("We could not find anything in our Database!")
+// TODO: (Important): The Stringified Version of the Array has to parsed in order for the Array Value to be used.
+
+// import { StringJSONPlayListArrayOne } from "../script";
+
+export function CheckDataBase() {
+  // * This IF Statements checks if the Local Database is Empty.
+
+  if (localStorage.length == 0) {
+    console.log("We could not find anything in our Database!");
     localStorage.clear();
   }
 
-  else if (localStorage.length >= 1){
-    console.log("We have found Poetential Playlists in the Database")
+  // * This ELSE IF Statement Checks if Local Database has 1 or more values. If any values have been detected this means that the LocalStorage has Playlist Data that can be restored
+  else if (localStorage.length >= 1) {
+    console.log("We have found Poetential Playlists in the Database");
     let UserDecision = window.prompt("Do You Wish to restore these Playlist?");
 
-    if (UserDecision == "yes"){
-      console.log(localStorage);
-      RestorePlayList();
+    if (UserDecision == "yes") {
     }
   }
 }
-
-function RestorePlayList(){
-  
-
-  // ! THIS CODE RESTORES PLAYLIST ONE!
-
-  let ArrayBase = localStorage.getItem("PlayArrayone");
-  console.log(ArrayBase);
-  // let IndexOfComma = ArrayBase.indexOf(",");
-  // let PlaylistName = ArrayBase.slice(IndexOfComma);
-  // console.log(ArrayBase); 
-  
-
-  // let TempIconValue = ImageValue;
-  // let TempNameValue = PlayListNameVall;
-
-  // // ! CONST VARIABLES CREATE ELEMENTS FOR DOM / PLAYLIST
-
-  // const NewPlayListContainer = document.createElement("div");
-  // const NewPlayListIcon = document.createElement("img");
-  // const NewPlayListName = document.createElement("h5");
-  // const ViewPlaylistText = document.createElement("h6");
-  // const ViewPlayListLink = document.createElement("a");
-
-  // // * MODIFIES PLAYLIST VALUES
-
-  // NewPlayListIcon.src = TempIconValue;
-  // NewPlayListName.innerHTML = TempNameValue;
-  // NewPlayListContainer.id = "PlaylistCSS";
-  // ViewPlaylistText.innerHTML = "Click to View!";
-  // ViewPlayListLink.href = "https://www.google.com";
-  // ViewPlayListLink.setAttribute("EventTarget", "_blank");
-
-  // // * DISPLAYS TO DOM
-
-  // document.body.appendChild(NewPlayListContainer);
-  // NewPlayListContainer.appendChild(NewPlayListIcon);
-  // NewPlayListContainer.appendChild(NewPlayListName);
-  // NewPlayListContainer.appendChild(ViewPlayListLink);
-  // ViewPlayListLink.appendChild(ViewPlaylistText);
-
-  
-}
-
-// function RestorePlayListTwo(){
-//   let ArrayBase = localStorage.getItem("PlayArrayTwo");
-//   let TempValue = ArrayBase.indexOf(",");
-//   let ImageValue = ArrayBase.slice(0, TempValue);
-//   let PlayListNameVall = ArrayBase.slice(TempValue + 1); 
-  
-
-//   let TempIconValue = ImageValue;
-//   let TempNameValue = PlayListNameVall;
-
-//   // ! CONST VARIABLES CREATE ELEMENTS FOR DOM / PLAYLIST
-
-//   const NewPlayListContainer = document.createElement("div");
-//   const NewPlayListIcon = document.createElement("img");
-//   const NewPlayListName = document.createElement("h5");
-//   const ViewPlaylistText = document.createElement("h6");
-//   const ViewPlayListLink = document.createElement("a");
-
-//   // * MODIFIES PLAYLIST VALUES
-
-//   NewPlayListIcon.src = TempIconValue;
-//   NewPlayListName.innerHTML = TempNameValue;
-//   NewPlayListContainer.id = "PlaylistCSS";
-//   ViewPlaylistText.innerHTML = "Click to View!";
-//   ViewPlayListLink.href = "https://www.google.com";
-//   ViewPlayListLink.setAttribute("EventTarget", "_blank");
-
-//   // * DISPLAYS TO DOM
-
-//   document.body.appendChild(NewPlayListContainer);
-//   NewPlayListContainer.appendChild(NewPlayListIcon);
-//   NewPlayListContainer.appendChild(NewPlayListName);
-//   NewPlayListContainer.appendChild(ViewPlayListLink);
-//   ViewPlayListLink.appendChild(ViewPlaylistText);
-// }
--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,29 @@
-// DOM Variables
+// * Imports the DBINIT FILE TO USE
 import { CheckDataBase } from "./LocalDb/DBINIT.js";
 
+// * DOM Variables
 const PlayListNameInput = document.getElementById("PlayListNameInput");
 const PlayListIconInput = document.getElementById("PlayListIconInput");
 const CreateBtn = document.getElementById("CreatePlayBtn");
 
+// * Each Playlist is Given an ID from 3-1, this variable gives it
 let PlayListID = 4;
 
+// ! Since Arrays cannot be passed in as its normal state the Playlist Data will be saved onto the "Non" Variables till conversion has happened
 
-let PlayListArrayOne = [];
-let PlayListArrayTwo = [""];
-let PlayListArrayThree = [""];
+let NonJSONPlayListArrayOne = [];
+let NonJSONPlayListArrayTwo = [];
+let NonJSONPlayListArrayThree = [];
 
+// ! These are the converted arrays name, once the data in NonJSON has been stringified it will be passed to this array before being processed to Local Database.
 
+let StringJSONPlayListArrayOne;
+
+// * The function below executes the Database checker to see if any Playlist exists before calling a decsion
 CheckDataBase();
 
 function OnCreateClick() {
-  // ! THIS FUNCTION DECREMENTS PLAYLIST ID TO MAKE SURE THE MAXIUMUM IS 3!
+  // ! This Function sets the maximum amount of Playlists to THREE!
 
   switch (PlayListID) {
     case 4:
@@ -48,7 +55,7 @@ function CreatePlayList() {
   let TempIconValue = PlayListIconInput.value;
   let TempNameValue = PlayListNameInput.value;
 
-  // ! CONST VARIABLES CREATE ELEMENTS FOR DOM / PLAYLIST
+  // * The Constant Variables create the Playlist Container and its content.
 
   const NewPlayListContainer = document.createElement("div");
   const NewPlayListIcon = document.createElement("img");
@@ -56,7 +63,7 @@ function CreatePlayList() {
   const ViewPlaylistText = document.createElement("h6");
   const ViewPlayListLink = document.createElement("a");
 
-  // * MODIFIES PLAYLIST VALUES
+  // * Once Playlist Container has been made the code belows starts modifiying its Data.
 
   NewPlayListIcon.src = TempIconValue;
   NewPlayListName.innerHTML = TempNameValue;
@@ -65,7 +72,7 @@ function CreatePlayList() {
   ViewPlayListLink.href = "../html/PlayOne.html";
   ViewPlayListLink.setAttribute("EventTarget", "_blank");
 
-  // * DISPLAYS TO DOM
+  // * The Code Below Displays the PlayList to the Document Model
 
   document.body.appendChild(NewPlayListContainer);
   NewPlayListContainer.appendChild(NewPlayListIcon);
@@ -78,33 +85,17 @@ function CreatePlayList() {
   switch (PlayListID) {
     case 3:
       NewPlayListContainer.className = "PlayListOne";
-      PlayListArrayOne.push(TempIconValue);
-      PlayListArrayOne.push(TempNameValue);
-      localStorage.setItem("PlayArrayone", PlayListArrayOne);
-      // console.log(PlayListArrayOne);
-      break;
+      NonJSONPlayListArrayOne.push(TempIconValue);
+      NonJSONPlayListArrayOne.push(TempNameValue);
+      StringJSONPlayListArrayOne = JSON.stringify(StringJSONPlayListArrayOne);
+      localStorage.setItem("PlayListOne", StringJSONPlayListArrayOne);
 
-    case 2:
-      NewPlayListContainer.className = "PlayListTwo";
-      PlayListArrayTwo.push(TempIconValue);
-      PlayListArrayTwo.push(TempNameValue);
-      localStorage.setItem("PlayArrayTwo", PlayListArrayOne);
-      break;
-
-    case 1:
-      NewPlayListContainer.className = "PlayListThree";
-      PlayListArrayThree.push(TempIconValue);
-      PlayListArrayThree.push(TempNameValue);
-      localStorage.setItem("PlayArrayThree", PlayListArrayThree);
       break;
   }
-
-  
-  
 }
 
-
-export { PlayListArrayOne, PlayListArrayTwo, PlayListArrayThree };
+// * This Exported the JSON String Arrays to move to Local Database Processing
+export { StringJSONPlayListArrayOne };
 
 CreateBtn.addEventListener("click", OnCreateClick);
 
@@ -112,4 +103,3 @@ document.getElementById("str").onclick = function () {
   localStorage.clear();
   console.warn("Cleared!");
 };
-
