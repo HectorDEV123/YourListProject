fdd2b2e2a5fb8f81c759dde21c250cb113f4ed70
HectorDEV123 <109687397+HectorDEV123@users.noreply.github.com>
2023-02-15 23:29:38 +0000

    s

--- javascript/PlayOne.js
+++ javascript/PlayOne.js
@@ -8,25 +8,46 @@ let AddSongBtn = document.getElementById("AddSongDOM");
 
 function ExeCreateSong(){
 
-  ExeCreateSong();
+  ExeCreateSong(CreateObject);
 }
 
 AddSongBtn.addEventListener("click", ExeCreateSong)
 
 
-function CreateSong(){
+function CreateSong(CallBack){
 
 
   // * Creates Song and Appends it to a designated container
 
-  
+  let SongIconVal = SongIcon.value;
+  let songNameVal = SongName.value;
+  let ArtistName = ArtistName.value;
 
+  
   const NewSongContainer = document.createElement("div");
   const SongIcon = document.createElement("img");
   const SongName = document.createElement("h5");
   const ArtistName = document.createElement("h5");
   const SongURL = null;
 
+  SongIcon.src = SongIconVal;
+  SongName.InnerHTML = SongNameVal;
+  ArtistName.InnerHTML = ArtistName;
 
+  
 
+  CallBack(SongIcon, SongName, ArtistName)
+
+}
+
+function CreateObject(SongIcon, SongName, ArtistName){
+
+  let SongObject = {
+
+    ArtistName: `${SongName}`,
+    SongIcon: `${SongIcon}`,
+    ArtistName: `${ArtistName}`
+    
+  }
+  
 }
